b2528f9ddeb6d57bc4519dc126f071c9eae598bc
CC <92929085+ChristineCharlotte@users.noreply.github.com>
2024-03-20 22:24:22 +0800

    Create page.tsx

---
+++ app/figma/card-1/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function Business1() {
+  const router = useRouter()
+
+  useEffect(()=>{
+    router.push("https://www.figma.com/community/file/1352285842257827240")
+  },[])
+
+  return (
+    <></>
+  )
+}
